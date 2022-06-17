fe18c4f1dafa05561f886b1477832e3cffd3b462
Anik <sikder35-1408@diu.edu.bd>
2022-06-17 12:25:11 +0600

    Linear search

---
+++ Array, Linear search vs Binary Search/linearSearch.js
@@ -0,0 +1,29 @@
+/**
+ * Linear search
+ */
+
+// let arr = [25, 'Anik', true, false, [], 27,
+// {}, function() {console.log('Hello');}];
+
+let arr = [25, 'Anik', true, false, [], 27,
+{},33, 89, 56];
+
+let key = 25;
+let flag = false;
+
+for(let i=0; i<arr.length;i++) {
+    // console.log(typeof arr[i]);
+    console.log(`I'm ${typeof arr[i]} at index ${i}`);
+
+    if(arr[i] === key) {
+        console.log('Key found at index', i);
+        flag = true;
+        break;
+    }
+}
+//console.log(flag);
+
+if(!flag) {
+    console.log('Not found');
+    //console.log(flag);
+}
